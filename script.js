3455b04f02961236f141912eb27ee3286dc92978
4xy3is <134912225+veeedu@users.noreply.github.com>
2025-10-25 13:36:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Site loaded');
+function loadFiles(jsonFile, folder, containerId) {
+  fetch(`public/${jsonFile}`)
+    .then(res => res.json())
+    .then(files => {
+      const container = document.getElementById(containerId);
+      files.forEach(file => {
+        const div = document.createElement('div');
+        div.className = 'file-card';
+
+        if(folder === 'images'){
+          const img = document.createElement('img');
+          img.src = `public/${folder}/${file}`;
+          img.alt = file;
+          div.appendChild(img);
+        }
+
+        const a = document.createElement('a');
+        a.href = `public/${folder}/${file}`;
+        a.download = file;
+        a.textContent = folder === 'images' ? 'Download' : file;
+        div.appendChild(a);
+
+        container.appendChild(div);
+      });
+    });
+}
+
+// Example usage in HTML:
+// loadFiles('images.json', 'images', 'image-container');
+// loadFiles('books.json', 'books', 'book-container');
+// loadFiles('templates.json', 'templates', 'template-container');
